925b92368ee2d4ddfa4ea63b075eb51c8d5dbc9f
hhkim0729 <heehkim@student.42seoul.kr>
2022-07-29 23:52:28 +0900

    [2738] 행렬 덧셈

---
+++ baekjoon/2738.py
@@ -0,0 +1,13 @@
+n, m = map(int, input().split())
+
+a = []
+for i in range(n) :
+	a.append(input().split())
+b = []
+for i in range(n) :
+	b.append(input().split())
+
+for i in range(n) :
+	for j in range(m) :
+		print(int(a[i][j]) + int(b[i][j]), end = ' ')
+	print()
