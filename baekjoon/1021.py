838bcc81f3d13606a5da2db86871fd6e8c5dfd64
hhkim0729 <heehkim@student.42seoul.kr>
2022-08-30 22:00:59 +0900

    [1021] 회전하는 큐

---
+++ baekjoon/1021.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+n, m = map(int, input().split())
+d = deque(range(1, n + 1))
+a = list(map(int, input().split()))
+
+count = 0
+for num in a :
+	if d.index(num) <= len(d) // 2 :
+		while d[0] != num :
+			d.rotate(-1)
+			count += 1
+		d.popleft()
+	else :
+		while d[0] != num :
+			d.rotate(1)
+			count += 1
+		d.popleft()
+print(count)
